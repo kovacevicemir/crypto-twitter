d0547395fa3a82e415560e6519a2cded61012f00
emirkovacevic <emir.kovacevic@jjryan.com.au>
2021-10-15 12:14:35 +1000

    creating redux actions and reduces cases for apis

--- client/src/store/actions/main.js
+++ client/src/store/actions/main.js
@@ -1,8 +1,12 @@
-import { TEST_COMMUNICATION } from "../actionTypes";
+import {
+  TEST_COMMUNICATION,
+  GET_COIN_PRICE_HISTORY,
+  GET_COIN_RANKING,
+  GET_TWEET
+} from "../actionTypes";
 import axios from "axios";
 
 const port = 5000;
-
 
 // ACTION Example:
 // 1st step
@@ -14,5 +18,32 @@ export const testAction = () => {
   return async (dispatch) => {
     let result = await axios.get(`http://localhost:${port}/test`);
     dispatch({ type: TEST_COMMUNICATION, payload: result.data });
+  };
+};
+
+export const getCoinRanking = (number_of_coins) => {
+  return async (dispatch) => {
+    let result = await axios.get(
+      `http://localhost:${port}/coinRoute/getCoinRanking?number_of_coins=${number_of_coins}`
+    );
+    dispatch({ type: GET_COIN_RANKING, payload: result.data });
+  };
+};
+
+export const getCoinPriceHistory = (uuid) => {
+  return async (dispatch) => {
+    let result = await axios.get(
+      `http://localhost:${port}/coinRoute/getCoinPriceHistory?uuid=${uuid}`
+    );
+    dispatch({ type: GET_COIN_PRICE_HISTORY, payload: result.data });
+  };
+};
+
+export const getTweet = async (keyword, number_of_tweets) => {
+  return async (dispatch) => {
+    let result = await axios.get(
+      `http://localhost:${port}/twitterRoute?keyword=${keyword}&number_of_tweets=${number_of_tweets}`
+    );
+    dispatch({ type: GET_TWEET, payload: result.data });
   };
 };
--- client/src/store/reducers/main.js
+++ client/src/store/reducers/main.js
@@ -1,4 +1,4 @@
-import { TEST_COMMUNICATION } from "../actionTypes";
+import { TEST_COMMUNICATION, GET_COIN_PRICE_HISTORY, GET_COIN_RANKING, GET_TWEET } from "../actionTypes";
 
 //Default state (memory)
 const initState = {
@@ -12,12 +12,21 @@ export const main = (state = initState, action) =>{
       return {
         data:action.payload
       };
-    //Example
-    //case TWITTER_NEWS:
-      // return{
-      //   ...state,
-      //   twitterNews: action.payload
-      // }
+    case GET_COIN_RANKING:
+      return {
+        ...state,
+        coin_ranking:action.payload
+      }
+    case GET_COIN_PRICE_HISTORY:
+      return {
+        ...state,
+        coin_price_history:action.payload
+      }
+    case GET_TWEET:
+      return {
+        ...state,
+        tweets:action.payload
+      }
     default:
       return state;
   }
