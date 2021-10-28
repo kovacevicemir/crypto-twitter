8f1adfbe747290de3dbacca6ee25dbf9a45352da
kmin983 <mmg004726@naver.com>
2021-10-28 22:44:36 +1000

    changed twitterRoute

--- server/routes/twitterRoute.js
+++ server/routes/twitterRoute.js
@@ -8,19 +8,17 @@ router.get("/tweet", (req, res) => {
     try {
         const topic = req.query.topic;
         const numTweets = req.query.nums;
-        let averageSentiment = 0;
         const sentiments = [];
         getTweet(topic, numTweets)
             .then((data) => {
                 data.forEach((tweet) => {
                     sentiments.push(getSentiment(tweet.tweet))
                 })
-                // Promise.all(sentiments)
-                //     .then((val) => {
-                //         averageSentiment = (val.reduce((prev, curr) => prev + curr), 0) / val.length);
-                        
-                //     })
-                res.status(200).json({"tweets": data, "averageSentiments": 0.75});
+                Promise.all(sentiments)
+                    .then((val) => {
+                        const averageSentiment = val.reduce((prev, curr) => prev + curr, 0) / val.length
+                        res.status(200).json({"tweets": data, "averageSentiments": averageSentiment});
+                    })
             })
         
     } catch (err) {
