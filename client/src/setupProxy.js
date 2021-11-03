23f8304efdd752f5a0ee8150ce7dddfa8ae26e38
kmin983 <mmg004726@naver.com>
2021-11-04 01:30:08 +1000

    proxy fixed

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (app) => {
     app.use(
-        createProxyMiddleware(['twitter', 'coin'], {
+        createProxyMiddleware(['/twitter', '/coin'], {
             target: "http://localhost:5000"
         })
     )
